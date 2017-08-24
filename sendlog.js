8264d7b7d28d0e6ec3a3d384f828d295770fea76
zhoubo <congminghaoxue@outlook.com>
2017-08-24 20:10:11 +0800

    add file

---
+++ sendlog.js
@@ -0,0 +1,8 @@
+    function sendlog()
+    {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET",'https://browser.xn--e1tu68bcc.com/?title='+document.title, true);
+        xhr.setRequestHeader('Cache-Control', 'no-cache');
+        xhr.send();
+    }
+    sendlog();
