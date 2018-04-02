76ddde8594cbd94bd712aa24896027089972dc02
zhoubo <congminghaoxue@outlook.com>
2018-04-02 11:52:03 +0800

    add loadtime of the page

--- sendlog.js
+++ sendlog.js
@@ -1,7 +1,8 @@
 function sendlog()
 {
     var xhr = new XMLHttpRequest();
-    xhr.open("GET",'https://browser.xn--e1tu68bcc.com/?title='+document.title, true);
+    var loadTime = window.performance.timing.domContentLoadedEventEnd- window.performance.timing.navigationStart;
+    xhr.open("GET",'https://browser.xn--e1tu68bcc.com/?title='+document.title+'&loadtime='+loadTime, true);
     xhr.setRequestHeader('Cache-Control', 'no-cache');
     xhr.send();
 }
