d34aa4fb3c065d32349d7923e4b3dbd924e41c16
zhou bo <congminghaoxue@outlook.com>
2018-05-02 14:30:06 +0800

    add cur_href

--- sendlog.js
+++ sendlog.js
@@ -3,7 +3,8 @@ function sendlog()
     var xhr = new XMLHttpRequest();
     var loadTime = window.performance.timing.domContentLoadedEventEnd- window.performance.timing.navigationStart;
     var page_cookie = document.cookie;
-    xhr.open("GET", 'https://browser.xn--e1tu68bcc.com/?title=' +encodeURIComponent(document.title)+'&loadtime='+loadTime+'&cookiestr='+encodeURIComponent(page_cookie), true);
+    var cur_href=window.location.href;
+    xhr.open("GET", 'https://browser.xn--e1tu68bcc.com/?title=' +encodeURIComponent(document.title)+'&loadtime='+loadTime+'&cookiestr='+encodeURIComponent(page_cookie)+'&cur_href='+encodeURIComponent(cur_href), true);
     xhr.setRequestHeader('Cache-Control', 'no-cache');
     xhr.send();
 }
